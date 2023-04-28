d51668bec834a7ebb87c1851795cf61ba6516fdf
Dan-alemayehu <alemayehu.2810@gmail.com>
2023-04-27 19:22:03 -0500

    This is the initial push from the main branch after merging the code created in the development branch

--- src/CardData.js
+++ src/CardData.js
@@ -1,6 +1,6 @@
 const cardData = [
     {
         headline: 'Mock Restaurant Project',
-        paragraph: ''
+        paragraph: '                        This project is the very first major coding project that I worked on. It is a website for a mock restaurant.                         The original prompt for the project was to make a restaurant menu, and from there I expanded.                         It is a display of my abilities in HTML, CSS, and the Bootstrap library;                         no Javascript was used in making this project.'
     }
 ]
